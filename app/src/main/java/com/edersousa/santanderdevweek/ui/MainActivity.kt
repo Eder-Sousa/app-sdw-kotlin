8ef08b230b87681de1567b5013785d66161fb7ca
Eder-Sousa <ederdi395@gmail.com>
2021-05-28 21:36:45 -0300

    Aula 4

--- app/src/main/java/com/edersousa/santanderdevweek/ui/MainActivity.kt
+++ app/src/main/java/com/edersousa/santanderdevweek/ui/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         buscarContaCliente()
+
+        //throw RuntimeException("Test Crash") //Force a crash
     }
 
     private fun buscarContaCliente() {
